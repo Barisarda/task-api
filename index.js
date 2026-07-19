2dd57c6035b883994c9261a77375e257539f905a
Barisarda <barisarda123@outlook.com>
2026-07-19 23:04:39 +0300

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let tasks = [
+  { id: 1, title: 'Learn Express', done: false },
+  { id: 2, title: 'Build CRUD API', done: false },
+  { id: 3, title: 'Push to GitHub', done: false }
+]
 
 app.get('/',(req, res) => {
     res.json({name : 'Task API', version: '1.0', endpoints: ['/tasks']})
@@ -12,4 +17,19 @@ app.get('/health',(req,res) =>{
 
 app.listen(port, () => {
     console.log('Server listening on port ${port}')
+})
+
+app.get('/tasks/:id',(req,res) =>{
+    const id = Number(req.params.id)
+    const task = tasks.find(t => t.id === id)
+    if (task) {
+        res.status(200).json(task)
+    } else {
+        res.status(404).json({ error: `Task ${id} not found`})
+    }
+    
+})
+
+app.get('/tasks', (req, res) => {
+res.json(tasks)
 })
